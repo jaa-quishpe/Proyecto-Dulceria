293d3ef01c2d68c9a2f351d312b34c55c5ae5c9a
Curipoma <alvarocuripoma@gmail.com>
2022-01-18 14:37:12 -0500

    carrito de compras

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,16 @@ import 'package:dulces/theme/app_theme.dart';
 import 'package:dulces/routes/app_routes.dart';
 import 'package:dulces/screen/screens.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
+
+  Map<String, dynamic> cart_shopping_list =
+      CartShoppingList.cart_shopping_list;
+  Map<String, dynamic> getCartShoppingList() {
+    return cart_shopping_list;
+  }
 
   @override
   Widget build(BuildContext context) {
--- lib/screen/carrito_screen.dart
+++ lib/screen/carrito_screen.dart
@@ -1,7 +1,21 @@
+import 'package:dulces/main.dart';
+import 'package:dulces/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 
-class CarritoScreen extends StatelessWidget {
+class CartShoppingList {
+  static Map<String, dynamic> cart_shopping_list = {};
+}
+
+class CarritoScreen extends StatefulWidget {
   const CarritoScreen({Key? key}) : super(key: key);
+
+  @override
+  State<CarritoScreen> createState() => _CarritoScreenState();
+}
+
+class _CarritoScreenState extends State<CarritoScreen> {
+  void getCartShoppingList() {}
+  Map<String, dynamic> cart_shopping_list = MyApp().getCartShoppingList();
 
   @override
   Widget build(BuildContext context) {
@@ -10,7 +24,32 @@ class CarritoScreen extends StatelessWidget {
         actions: [],
         title: Text('Carrito'.toUpperCase()),
       ),
-      body: const Text("Estás en el carrito de compras !"),
+      body: cart_shopping_list.isNotEmpty
+          ? ListView.separated(
+              itemBuilder: (context, index) => Container(
+                decoration: const BoxDecoration(color: Colors.white),
+                child: ListTile(
+                  title: Text(cart_shopping_list['name']),
+                  subtitle: Text(cart_shopping_list['data'][index]['name']),
+                  trailing: Text(cart_shopping_list['data'][index]['price']),
+                ),
+              ),
+              itemCount: cart_shopping_list.length,
+              separatorBuilder: (_, __) => const Divider(),
+            )
+          : Center(
+              child: Container(
+                padding: const EdgeInsets.symmetric(
+                    vertical: 10.0, horizontal: 20.0),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(5.0)),
+                child: const Text(
+                  'Aún no tienes productos en el carrito',
+                  style: TextStyle(color: AppTheme.primary),
+                ),
+              ),
+            ),
     );
   }
 }
